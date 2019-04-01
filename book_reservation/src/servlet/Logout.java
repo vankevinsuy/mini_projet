c75191877a4bb388ceb1b694c834f0b2d886ab6c
vankevinsuy <vankevinsuy@gmail.com>
2019-04-01 20:38:24 +0200

    lien entre les pages (jusqu'à welcome.jsp) établis

--- book_reservation/src/servlet/Logout.java
+++ book_reservation/src/servlet/Logout.java
@@ -13,7 +13,10 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/Logout")
 public class Logout extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.getRequestDispatcher("/goodbye.html").forward(request, response);
 	}
