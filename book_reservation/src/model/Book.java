a0c1fcdd9d2ffd600d7d07d8f12bf5d2d29ec550
vankevinsuy <vankevinsuy@gmail.com>
2019-04-01 12:55:32 +0200

    tout est là

---
+++ book_reservation/src/model/Book.java
@@ -0,0 +1,39 @@
+package model;
+
+public class Book {
+	private String name;
+	private String image_Link;
+	private boolean booked;
+	
+	
+	public Book(String name, String image_Link) {
+		super();
+		this.name = name;
+		this.image_Link = image_Link;
+		this.booked = false;
+	}
+
+
+	public boolean isBooked() {
+		return booked;
+	}
+
+
+	public void setBooked(boolean booked) {
+		this.booked = booked;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public String getImage_Link() {
+		return image_Link;
+	}
+	
+	
+	
+}
+
---
+++ book_reservation/src/servlet/Login.java
@@ -0,0 +1,38 @@
+package servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.User;
+
+/**
+ * Servlet implementation class Login
+ */
+@WebServlet("/Login")
+public class Login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		User prof = new User("prof", "1234" );
+		User eleve = new User("eleve", "invite" );
+		
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
